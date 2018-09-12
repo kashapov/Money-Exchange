eaf487d35a46c09c936660bf58df60b3744bbe2a
Andrey Kashapov <kashapovandrey@gmail.com>
2018-09-12 14:18:02 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,43 +1,61 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-  // Return an object containing the minimum number of coins needed to make change
-  var monets = {};
+  // Return an object containing the minimum number of coins needed to make change  
+  let coins = {};
+  let money, h, q, d, n, p;
 
   if (currency < 10000) {
-    if ((currency % 50) >= 0) {
-      h = Math.floor(currency/50);
-      currency -= h*50;
-      if (h > 0)
-        monets['H'] = h;
-    }
-    if ((currency % 25) >= 0) {
-      q = Math.floor(currency/25);
-      currency -= q*25;
-      if (q > 0)
-        monets['Q'] = q;
-    }
-    if ((currency % 10) >= 0) {
-      d = Math.floor(currency/10);
-      currency -= d*10;
-      if(d > 0)
-        monets['D'] = d;
-    }
-    if ((currency % 5) >= 0) {
-      n = Math.floor(currency/5);
-      currency -= n*5;
-      if(n > 0)
-        monets['N'] = n;
-    }
-    
-    
-    p = currency;
-    if(p > 0)
-      monets['P'] = p;
+    money = currency;
 
-    
+    if ((money % 50) >= 0) {
+      h = Math.floor(money / 50);
+      money -= h * 50;
+
+      if (h > 0) {
+        coins['H'] = h;
+      }
+
+    }
+
+    if ((money % 25) >= 0) {
+      q = Math.floor(money / 25);
+      money -= q * 25;
+
+      if (q > 0) {
+        coins['Q'] = q;
+      }
+
+    }
+
+    if ((money % 10) >= 0) {
+      d = Math.floor(money / 10);
+      money -= d * 10;
+
+      if (d > 0) {
+        coins['D'] = d;
+      }
+
+    }
+
+    if ((money % 5) >= 0) {
+      n = Math.floor(money / 5);
+      money -= n * 5;
+
+      if (n > 0) {
+        coins['N'] = n;
+      }
+
+    }
+
+    p = money;
+
+    if (p > 0) {
+      coins['P'] = p;
+    }
+
   }
   else
-    monets['error'] = "You are rich, my friend! We don't have so much coins for exchange";
+    coins['error'] = "You are rich, my friend! We don't have so much coins for exchange";
 
-  return monets;
+  return coins;
 }
